5d5af8b5c164f22af83f61f9d4dbedf1d26b2617
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-03-04 16:02:37 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 Leaflet.spin
 ============
+
+Shows a nice spin cursor on the map.
+
+Requires [Spin.js](http://fgnass.github.com/spin.js/)
+
+Usage
+-----
+
+```
+
+    var layer = L.GeoJSON(null).addTo(map);
+    
+    layer.fire('loading');
+    $.getJSON('http://server/path.geojson', function (data) {
+        layer.fire('loaded');
+        layer.addData(data);
+    });
+
+```
---
+++ leaflet.spin.js
@@ -0,0 +1,39 @@
+L.SpinMapMixin = {
+    spin: function (state) {
+        var state = !!state;
+        if (state) {
+            // start spinning !
+            if (!this._spinner) {
+                this._spinner = new Spinner().spin(this._container);
+                this._spinning = 0;
+            }
+            this._spinning++;
+        }
+        else {
+            this._spinning--;
+            if (this._spinning <= 0) {
+                // end spinning !
+                if (this._spinner) {
+                    this._spinner.stop();
+                    this._spinner = null;
+                }
+            }
+        }
+    }
+};
+
+L.Map.include(L.SpinMapMixin);
+
+L.Map.addInitHook(function () {
+    this.on('layeradd', function (e) {
+        if (e.layer.loading) this.spin(true);
+        e.layer.on('loading', function () { this.spin(true) }, this);
+        e.layer.on('loaded',  function () { this.spin(false) }, this);
+    }, this);
+    this.on('layerremove', function (e) {
+        if (e.layer.loading) this.spin(false);
+        e.layer.off('loaded');
+        e.layer.off('loading');
+    }, this);
+});
+
