210ecfa39441307839a8aef31904063edc0ef23a
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-04-16 16:27:50 +0200

    Safety check if layer does include Event mixin (like LayerGroup)

--- leaflet.spin.js
+++ leaflet.spin.js
@@ -28,12 +28,14 @@ L.Map.addInitHook(function () {
     this.on('layeradd', function (e) {
         // If added layer is currently loading, spin !
         if (e.layer.loading) this.spin(true);
+        if (typeof e.layer.on != 'function') return;
         e.layer.on('data:loading', function () { this.spin(true) }, this);
         e.layer.on('data:loaded',  function () { this.spin(false) }, this);
     }, this);
     this.on('layerremove', function (e) {
         // Clean-up
         if (e.layer.loading) this.spin(false);
+        if (typeof e.layer.on != 'function') return;
         e.layer.off('data:loaded');
         e.layer.off('data:loading');
     }, this);
