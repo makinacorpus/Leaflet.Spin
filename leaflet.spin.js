6391622fb3528059c2ba8b1f30c6ce3de024b2d5
Sbats <simon.bats12@gmail.com>
2016-11-30 11:57:28 +0100

    Fix plugin export to extend Leaflet instead of exposing methods

--- leaflet.spin.js
+++ leaflet.spin.js
@@ -1,11 +1,31 @@
-(function (window) {
+(function (factory, window) {
+    // define an AMD module that relies on 'leaflet'
+    if (typeof define === 'function' && define.amd) {
+        define(['leaflet'], function (L) {
+            factory(L);
+        });
 
+    // define a Common JS module that relies on 'leaflet'
+    } else if (typeof exports === 'object') {
+        module.exports = function (L) {
+            if (L === undefined) {
+                L = require('leaflet');
+            }
+            factory(L);
+            return L;
+        };
+    // attach your plugin to the global 'L' variable
+    } else if (typeof window !== 'undefined' && window.L) {
+        factory(window.L);
+    }
+}(function leafletSpinFactory(L) {
     var SpinMapMixin = {
         spin: function (state, options) {
             if (!!state) {
                 // start spinning !
                 if (!this._spinner) {
-                    this._spinner = new Spinner(options).spin(this._container);
+                    this._spinner = new Spinner(options)
+                        .spin(this._container);
                     this._spinning = 0;
                 }
                 this._spinning++;
@@ -27,34 +47,23 @@
         this.on('layeradd', function (e) {
             // If added layer is currently loading, spin !
             if (e.layer.loading) this.spin(true);
-            if (typeof e.layer.on != 'function') return;
-            e.layer.on('data:loading', function () { this.spin(true); }, this);
-            e.layer.on('data:loaded',  function () { this.spin(false); }, this);
+            if (typeof e.layer.on !== 'function') return;
+            e.layer.on('data:loading', function () {
+                this.spin(true);
+            }, this);
+            e.layer.on('data:loaded',  function () {
+                this.spin(false);
+            }, this);
         }, this);
         this.on('layerremove', function (e) {
             // Clean-up
             if (e.layer.loading) this.spin(false);
-            if (typeof e.layer.on != 'function') return;
+            if (typeof e.layer.on !== 'function') return;
             e.layer.off('data:loaded');
             e.layer.off('data:loading');
         }, this);
     };
 
-    // define an AMD module that relies on 'leaflet'
-    if (typeof define === 'function' && define.amd) {
-        define(['leaflet'], SpinMapMixin);
-        define(['leaflet'], SpinMapInitHook);
-
-    // define a Common JS module that relies on 'leaflet'
-    } else if (typeof exports === 'object') {
-        module.exports = {
-          SpinMapMixin: SpinMapMixin,
-          SpinMapInitHook: SpinMapInitHook
-        };
-    }
-    // attach your plugin to the global 'L' variable
-    if (typeof window !== 'undefined' && window.L) {
-        L.Map.include(SpinMapMixin);
-        L.Map.addInitHook(SpinMapInitHook);
-    }
-}(window));
+    L.Map.include(SpinMapMixin);
+    L.Map.addInitHook(SpinMapInitHook);
+}, window));
